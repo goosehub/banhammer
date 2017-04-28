825248702fb07302c011055f3135af8bb69957d1
Goose <goosepostbox@gmail.com>
2017-04-27 22:17:15 -0400

    htaccess and ssl off for migration change
    small orange to liquid web.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -37,6 +37,7 @@ function auth() {
 }
 
 function force_ssl() {
+    return false;
     if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != "on") {
         $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
         redirect($url);
